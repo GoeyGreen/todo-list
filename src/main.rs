a2a01aae4e1cc31cf89d84e7d0a43414930b51cd
GoeyGreen <114379647+GoeyGreen@users.noreply.github.com>
2025-05-04 23:40:09 -0700

    Updated Styling on buttons

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
-use iced::{Alignment, Element, Length, Subscription};
-use iced::widget::{button, horizontal_space, text, text_input, Column, Container, Row};
+use std::time::{Duration, Instant};
+
+use iced::{Alignment, Color, Element, Length, Subscription, Theme};
+use iced::widget::{button, horizontal_space, text, text_input, Column, Container, Row, Scrollable};
 use chrono::prelude::{DateTime, Local};
 
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, PartialEq)]
 struct ToDo{
     time: DateTime<Local>,
     clock: String,
     tasks: Vec<String>,
     add: bool,
+    complete: i32,
+    removed: i32,
+    start: Instant,
+    stopwatch: Duration,
+    stop_string: String,
+    last_time: Duration,
+    last_string: String,
+}
+
+impl Default for ToDo {
+    fn default() -> Self {
+        Self {
+            time: Local::now(),
+            clock: String::new(),
+            tasks: Vec::new(),
+            add: false,
+            complete: 0,
+            removed: 0,
+            start:Instant::now(),
+            stopwatch:Duration::new(0,0),
+            stop_string: String::new(),
+            last_time:Duration::new(0,0),
+            last_string: String::new(),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -17,7 +44,7 @@ enum Message{
     Cancel,
     End,
     AddTask(String, i32),
-    RemoveTask(i32),
+    RemoveTask(i32, bool),
     Tick,
 
 }
@@ -31,28 +58,61 @@ impl ToDo {
                 .push(horizontal_space())
                 .push(button("Reset").on_press(Message::Reset)).spacing(10));
         main = main.push(text("To Do List: ").size(20)).push(text(&self.clock).size(16)).push(text("").size(30));
+        let mut tasks: Column<'_, Message> = Column::new().align_x(Alignment::Center).width(Length::Fill).padding(20).spacing(10).into();
         for (index, task) in self.tasks.clone().into_iter().enumerate(){
             if index != self.tasks.len() - 1 {
-                main = main.push(
+                tasks = tasks.push(
                     Container::new(
-                        Row::new().push(button("Complete").on_press(Message::RemoveTask(index as i32)))
+                        Row::new().push(button("Complete").on_press(Message::RemoveTask(index as i32, true)))
                         .push(text(task).size(16))
                         .push(horizontal_space())
-                        .push(button("Remove").on_press(Message::RemoveTask(index as i32))).spacing(20).width(Length::Fill)
+                        .push(button("Remove").on_press(Message::RemoveTask(index as i32, false)).style(|_: &Theme, status| {
+                            match status {
+                                button::Status::Active => {
+                                    button::Style::default()
+                                       .with_background(Color::from_rgb(255.0, 0.0, 0.0),)
+                                }
+                                _ => {
+                                    let mut style = button::Style::default()
+                                       .with_background(Color::from_rgb(70.0, 0.0, 0.0),);
+                                    style.text_color = Color::from_rgb(255.0, 255.0, 255.0);
+                                    style
+                                },
+                            }
+                        }))
+                        .spacing(20).width(Length::Fill)
                     )
                 );
             } else if !self.add && index == self.tasks.len() - 1 {
-                main = main.push(
+                tasks = tasks.push(
                     Container::new(
-                        Row::new().push(button("Complete").on_press(Message::RemoveTask(index as i32)))
+                        Row::new().push(button("Complete").on_press(Message::RemoveTask(index as i32, true)))
                         .push(text(task).size(16))
                         .push(horizontal_space())
-                        .push(button("Remove").on_press(Message::RemoveTask(index as i32))).spacing(20).width(Length::Fill)
+                        .push(button("Remove").on_press(Message::RemoveTask(index as i32, false)).style(|_: &Theme, status| {
+                            match status {
+                                button::Status::Active => {
+                                    let mut style = button::Style::default()
+                                       .with_background(Color::from_rgb(255.0, 0.0, 0.0),);
+                                    style.text_color = Color::from_rgb(255.0, 255.0, 255.0);
+                                    style
+                                }
+                                _ => {
+                                    let mut style = button::Style::default()
+                                       .with_background(Color::from_rgb(70.0, 0.0, 0.0),);
+                                    style.text_color = Color::from_rgb(255.0, 255.0, 255.0);
+                                    style
+                                },
+                            }
+                        }))
+                        .spacing(20).width(Length::Fill)
                     )
                 );
             }
         }
         
+        main = main.push(Scrollable::new(tasks));
+        main = main.push(Row::with_children(vec![text(format!("Current Task: {}", self.stop_string)).into(), text(format!("Last Task: {}", self.last_string)).into()]).spacing(20));
         if self.add {
             main = main.push(Container::new(
                 text_input("New Task ...", &self.tasks[self.tasks.len() - 1])
@@ -85,13 +145,26 @@ impl ToDo {
             Message::AddTask(task, index) => {
                 self.tasks[index as usize] = task;
             },
-            Message::RemoveTask(task_num) => {
+            Message::RemoveTask(task_num, completed) => {
                 self.tasks.remove(task_num as usize);
+                self.last_time = self.stopwatch;
+                self.last_string = format_duration(self.last_time);
+                self.stopwatch = Duration::new(0,0);
+                self.start = Instant::now();
+            
+                if completed {
+                    self.complete += 1;
+                } else {
+                    self.removed += 1;
+                }
             },
             Message::Tick => {
                 if Local::now() != self.time {
                     self.time = Local::now();
-                    self.clock = self.time.format("%d/%m/%Y %H:%M:%S").to_string()
+                    self.clock = self.time.format("%d/%m/%Y %H:%M:%S").to_string();
+                    self.stopwatch = self.start.elapsed();
+                    self.stop_string = format_duration(self.stopwatch);
+
                 }
             },
         }
@@ -105,6 +178,14 @@ impl ToDo {
 
 }
 
+fn format_duration(duration: Duration) -> String {
+    let secs = duration.as_secs();
+    let hours = secs / 3600;
+    let minutes = (secs % 3600) / 60;
+    let seconds = secs % 60;
+
+    format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+}
 
 
 fn main() -> iced::Result<> {
